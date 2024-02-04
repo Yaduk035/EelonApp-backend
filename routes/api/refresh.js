d4a4a95ef2a53953245c219c1de1c72871e4ff97
Yadu Krishna <yaduk946@gmail.com>
2024-02-04 22:42:41 +0530

    Added refresh token routes

--- routes/api/refresh.js
+++ routes/api/refresh.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const refreshTokenController = require("../../controllers/refreshTokenController");
 
 router.route("/student").get(refreshTokenController.handleStudentRefreshToken);
-router.route("/staff").get(refreshTokenController.handleStudentRefreshToken);
-router.route("/admin").get(refreshTokenController.handleStudentRefreshToken);
+router.route("/staff").get(refreshTokenController.handleStaffRefreshToken);
+router.route("/admin").get(refreshTokenController.handleAdminRefreshToken);
 
 module.exports = router;
