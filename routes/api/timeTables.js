bf61eb83463cbda39960bac773ef54b3ab6c1709
Yadu Krishna <yaduk946@gmail.com>
2024-02-18 17:59:33 +0530

    Removed routes for timetable templates

--- routes/api/timeTables.js
+++ routes/api/timeTables.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 const timetableController = require("../../controllers/timetableController");
 
-router
-  .route("/class/template/editarray/:id")
-  .put(timetableController.addTimetableToArray)
-  .delete(timetableController.deleteTimetableFromArray);
+// router
+//   .route("/class/template/editarray/:id")
+//   .put(timetableController.addTimetableToArray)
+//   .delete(timetableController.deleteTimetableFromArray);
 
-router
-  .route("/class/template/dropdowns")
-  .get(timetableController.getTemplateDropdowns);
+// router
+//   .route("/class/template/dropdowns")
+//   .get(timetableController.getTemplateDropdowns);
 
-router
-  .route("/class/template/:id")
-  .get(timetableController.getTemplateByTemplateId)
-  .put(timetableController.updateTemplateById)
-  .delete(timetableController.deleteTemplateById);
+// router
+//   .route("/class/template/:id")
+//   .get(timetableController.getTemplateByTemplateId)
+//   .put(timetableController.updateTemplateById)
+//   .delete(timetableController.deleteTemplateById);
 
 router
   .route("/classwise/classid/:id")
@@ -32,9 +32,9 @@ router
   .get(timetableController.getClasswiseTimetable)
   .delete(timetableController.deleteClasswiseTimetable);
 
-router
-  .route("/class/template")
-  .get(timetableController.getTemplates)
-  .post(timetableController.addTemplate);
+// router
+//   .route("/class/template")
+//   .get(timetableController.getTemplates)
+//   .post(timetableController.addTemplate);
 
 module.exports = router;
