8fb11727b261109ab6b6ca3d5099470ab3dfc4d0
Yadu Krishna <yaduk946@gmail.com>
2024-03-05 16:54:37 +0530

    Added post route for complaints

--- models/Transportation/BusSchema.js
+++ models/Transportation/BusSchema.js
@@ -1,23 +1,56 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const busSchema = new Schema({
-  busNo: String,
-  rgNo: {
-    type: String,
-    trim: true,
+const complaintsArray = new Schema(
+  {
+    title: String,
+    description: String,
+    status: {
+      default: "not resolved",
+      type: String,
+    },
   },
-  vehicleType: String,
-  vehicleModel: String,
-  seatNo: String,
-  mileage: String,
-  yearOfMade: String,
-  driverName: String,
-  helper: String,
-  status: {
-    default: "Active",
-    type: String,
+  { timestamps: true }
+);
+
+const busSchema = new Schema(
+  {
+    busNo: String,
+    rgNo: {
+      type: String,
+      trim: true,
+    },
+    academicYear: String,
+    vehicleType: String,
+    vehicleModel: String,
+    seatNo: String,
+    mileage: String,
+    yearOfMade: String,
+    driverName: String,
+    helper: String,
+    FC: {
+      public_id: {
+        type: String,
+      },
+      url: {
+        type: String,
+      },
+    },
+    RC: {
+      public_id: {
+        type: String,
+      },
+      url: {
+        type: String,
+      },
+    },
+    status: {
+      default: "Active",
+      type: String,
+    },
+    complaints: [complaintsArray],
   },
-});
+  { timestamps: true }
+);
 const bus = mongoose.model("BusData", busSchema);
 module.exports = bus;
