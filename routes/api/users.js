759f6527e15f59f425da6667d3ab7c65506e8560
Yadu Krishna <yaduk946@gmail.com>
2024-03-09 13:31:38 +0530

    Few changes in user routes file

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,58 +1,34 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../../controllers/userController");
-const notificationController = require("../../controllers/notificationController");
+const userController = require('../../controllers/userController');
+const notificationController = require('../../controllers/notificationController');
+
+/////////////  '/users/*'  /////////////
 
 //////////////    Admin routes     ////////////////
-router
-  .route("/admin")
-  .get(userController.getAdmins)
-  .post(userController.addAdmin);
+router.route('/admin').get(userController.getAdmins).post(userController.addAdmin);
 // router.route("/student/:id").delete(userController.deleteAdmin);
 
 //////////////    Student routes     ////////////////
-router
-  .route("/student/issuelibrarycard/:id")
-  .put(userController.issueLibCard)
-  .get(userController.getStudentByNameIssueLib);
+router.route('/student/issuelibrarycard/:id').put(userController.issueLibCard).get(userController.getStudentByNameIssueLib);
 
-router.route("/student/pagination").get(userController.getStudentsByLimit);
-router.route("/student/filter").put(userController.filterStudentsByclass);
+router.route('/student/pagination').get(userController.getStudentsByLimit);
+router.route('/student/filter').put(userController.filterStudentsByclass);
 
-router.route("/student/filterbydata").put(userController.studentFiltering);
+router.route('/student/filterbydata').put(userController.studentFiltering);
 
-router
-  .route("/student")
-  .get(userController.getStudents)
-  .post(userController.addStudent);
-router
-  .route("/student/:id")
-  .delete(userController.deleteStudent)
-  .get(userController.getStudentById)
-  .put(userController.updateStudent);
-router.route("/student/search/:id").get(userController.getStudentByName);
+router.route('/student').get(userController.getStudents).post(userController.addStudent);
+router.route('/student/:id').delete(userController.deleteStudent).get(userController.getStudentById).put(userController.updateStudent);
+router.route('/student/search/:id').get(userController.getStudentByName);
 
 //////////////    Staff routes     ////////////////
-router
-  .route("/staff/notifications")
-  .get(notificationController.getAllNotifications)
-  .post(notificationController.postNotifications);
-router
-  .route("/staff/notifications/:id")
-  .get(notificationController.getNotificationsByClass)
-  .delete(notificationController.deleteNotification);
+router.route('/staff/notifications').get(notificationController.getAllNotifications).post(notificationController.postNotifications);
+router.route('/staff/notifications/:id').get(notificationController.getNotificationsByClass).delete(notificationController.deleteNotification);
 
-router.route("/staff/filter").put(userController.staffFiltering);
+router.route('/staff/filter').put(userController.staffFiltering);
 
-router
-  .route("/staff")
-  .get(userController.getUsers)
-  .post(userController.addStaff);
-router
-  .route("/staff/:id")
-  .delete(userController.deleteStaff)
-  .get(userController.getIndividualStaff)
-  .put(userController.updateStaff);
+router.route('/staff').get(userController.getUsers).post(userController.addStaff);
+router.route('/staff/:id').delete(userController.deleteStaff).get(userController.getIndividualStaff).put(userController.updateStaff);
 // router.route('/staff')
 
 module.exports = router;
